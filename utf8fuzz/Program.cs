e2f14dd6e5e94894e5fafeaa30570ee530f4480e
Levi Broderick <levib@microsoft.com>
2019-04-24 11:31:43 -0700

    Add utf8fuzz project

---
+++ utf8fuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace utf8fuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
